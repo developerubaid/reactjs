b9d65dc70febbf637bf6c268de8991bb0d802c7c
Hafiz Ubaidullah Anwar <developerubaid@gmail.com>
2021-10-06 18:38:10 +0500

    Hello World
    This project just prints hello world in React JS and does nothing to it.

--- src/App.js
+++ src/App.js
@@ -1,33 +1,8 @@
 // import logo from "./logo.svg";
 import "./App.css";
-import Component1 from "./Components/Component1";
-import Component2 from "./Components/Component2";
-import Component3 from "./Components/Component3";
 
 function App() {
-  return (
-    <div className="App">
-      <h1>In the name of Allah</h1>
-      <Component1 />
-      <Component2 />
-      <Component3 />
-
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
-    </div>
-  );
+  return <div className="App">Hello World!</div>;
 }
 
 export default App;
---
+++ src/Components/ItemDescription.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function ItemDescription({ name, description }) {
+  return (
+    <div>
+      <p>{name}</p>
+      <p>
+        <i>{description}</i>
+      </p>
+    </div>
+  );
+}
+
+export default ItemDescription;
